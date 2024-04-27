b177c601f65f4f2a6a5be843c19fe16e8e1edc40
msdarby <maud_darby@yahoo.com>
2024-04-27 19:59:48 +0100

    Day_2_Project

--- Day_2_Project_tip-calculator.py
+++ Day_2_Project_tip-calculator.py
@@ -1,10 +1,10 @@
-print("Welcome to the tip calcular")
-bill = int(input("What was the total bill?: "))
-tip = input("How much tip would you like to give? 10, 12, 15: ")
-tip_percent = int(tip)/100
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill?: £"))
+tip = input("How much tip would you like to give? 10, 12, 15 n.b. please dont add a % sign: ")
+tip_percent = float(tip)/100
 total = bill + (bill * tip_percent)
 people = int(input("how many people to split the bill?: "))
 
 split = round(total / people, 2)
 
-print("Each person should pay: $" + str(split))
+print("Each person should pay: £" + str(split))
