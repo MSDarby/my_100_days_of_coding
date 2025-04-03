6cdb8872e72dac94cf7c8b513ad84485c359eaea
Maud <94403828+MSDarby@users.noreply.github.com>
2025-04-04 00:00:24 +0100

    Update and rename Day_5_Project_Password_Generator.py to Day_5_Project_Password_Generator1.py

--- Day_5_Project_Password_Generator1.py
+++ Day_5_Project_Password_Generator1.py
@@ -8,12 +8,12 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password_list = []
+password = ""
 for num in range(1, nr_letters+1):
-    password_list.append(random.choice(letters))
+    password+=random.choice(letters)
 for num in range(1, nr_symbols+1):
-    password_list.append(random.choice(symbols))
+    password+=random.choice(symbols)
 for num in range(1, nr_numbers+1):
-    password_list.append(random.choice(numbers))
+    password+=random.choice(numbers)
 
-print(password_list)
+print(password)
